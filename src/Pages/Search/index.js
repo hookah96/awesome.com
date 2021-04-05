d4a4a9460b20383f599a28bcd3a961b35d02ea2a
hookah96 <mak96grg@gmail.com>
2021-04-05 17:53:41 +0300

    fix warnings

--- src/Pages/Search/index.js
+++ src/Pages/Search/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Card from '../../components/Card';
 import { setSEOkeywords } from '../../utils/search';
 
-import { container, searchBar, gridCards, positionCard } from './style';
+import { container, gridCards, positionCard } from './style';
 
 const SearchPage = ({
   products,
